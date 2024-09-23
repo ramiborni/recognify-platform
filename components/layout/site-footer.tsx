c52b7bf8667fbe7cc803689413579034b03248f7
Rami Borni <borniahmedrami@gmail.com>
2024-09-23 17:03:44 +0100

    update copyright

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -38,38 +38,10 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
 
       <div className="border-t py-4">
         <div className="container flex max-w-6xl items-center justify-between">
-          {/* <span className="text-muted-foreground text-sm">
-            Copyright &copy; 2024. All rights reserved.
-          </span> */}
-          <p className="text-left text-sm text-muted-foreground">
-            Built by{" "}
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              mickasmt
-            </Link>
-            . Hosted on{" "}
-            <Link
-              href="https://vercel.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Vercel
-            </Link>
-            . Illustrations by{" "}
-            <Link
-              href="https://popsy.co"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Popsy
-            </Link>
-          </p>
+          <span className="text-sm text-muted-foreground">
+            Copyright &copy; 2024. All rights reserved to Recognify and Vendonet LTD.
+          </span> 
+          
 
           <div className="flex items-center gap-3">
             <Link
