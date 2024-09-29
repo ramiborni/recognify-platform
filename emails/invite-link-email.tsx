33456243ea9f0b5c2df2c5b961d909f881ab6866
Rami Borni <borniahmedrami@gmail.com>
2024-09-29 21:23:31 +0100

    fix email link

--- emails/invite-link-email.tsx
+++ emails/invite-link-email.tsx
@@ -45,7 +45,7 @@ export const InviteLinkEmail = ({
             <Section className="my-5 text-center">
               <Button
                 className="inline-block rounded-md bg-zinc-900 px-4 py-2 text-base text-white no-underline"
-                href={`https://recognify.com/invite/${invitationToken}`}
+                href={`https://recognify.io/invite/${invitationToken}`}
               >
                 Activate Account
               </Button>
