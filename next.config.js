7b2dd5995837f61852de7de8bfb09148e1d4f20d
Rami Borni <borniahmedrami@gmail.com>
2024-10-06 01:10:07 +0100

    update cors

--- next.config.js
+++ next.config.js
@@ -6,6 +6,27 @@ import("./env.mjs");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
@@ -22,12 +43,12 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "media.tenor.com"
+        hostname: "media.tenor.com",
       },
       {
         protocol: "https",
-        hostname: "media1.giphy.com"
-      }
+        hostname: "media1.giphy.com",
+      },
     ],
   },
   experimental: {
